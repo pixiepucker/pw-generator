ba06dfc57d887112e2fb6e5fd28821beee36f99b
pixiepucker <autumnoffic@gmail.com>
2022-02-25 00:02:23 -0600

    added constants to hold arrays of characters

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,25 @@
 // Assignment code here
 function generatePassword() {
-
+  const pwLowers = [
+    "a","b","c", "d", "f", "e", "g", "h", "i", 
+    "j", "k", "l", "m", "n", "o", "p", "q", "r", 
+    "s", "t", "u", "v", "w", "x", "y", "z"
+  ];
+  const pwUppers = [
+    'A','B','C','D','E','F','G','H','I','J','K','L','M','N','O',
+    'P','Q','R','S','T','U','V','W','X','Y','Z'
+  ];
+  const pwNumbers = [
+    '0','1','2','3','4','5','6','7','8','9'
+  ];
+  const pwSpecChar = [
+    '!','#','$','%','&','(',')','*','@','?','_','+','^'
+  ];
   //when button is clicked(generateBtn), prompts are presented:
   //  1. pw length is between 8 - 128
   var pwLength = prompt("Please input a length between 8 and 128.");
   //  2. ask to include: lowercase, uppercase, numerics, special characters
   var pwLowercase = confirm("Include lowercase?");
-    if (confirm("Include lowercase?") == true) {
-
-    }
   var pwUppercase = confirm("Include uppercase?");
   var pwNumerics = confirm("Include numbers?");
   var pwSpecials = confirm("Include special characters?");
